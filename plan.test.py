0445b862ba2b2e6df12520e0fbd681e94087c6a2
denshade <lieven.vaneeckhaute@gmail.com>
2022-07-18 12:06:26 +0200

    simple implementation

--- plan.test.py
+++ plan.test.py
@@ -1,27 +1,30 @@
 import unittest
-from state import get_plan
-from state import State, Transition
-
+from state import get_transitions
+from state import Transition, populate_transitions
 
 class TestStringMethods(unittest.TestCase):
 
     def test_plan_not_empty_for_one_state(self):
-        init = State("init", None, None, None)
         transition1 = Transition("START", "init", "script1.sh", 12)
-        first_transition = get_plan(["init"], [init], [transition1])[0]
+        transition2 = Transition("init", "START", "script2.sh", 12)
+
+        first_transition = get_transitions(["init"], [transition1, transition2])[0]
         self.assertEqual(first_transition.from_state, "START")
         self.assertEqual(first_transition.to_state, "init")
         self.assertEqual(first_transition.script_to_run, "script1.sh")
 
     def test_plan_second_state(self):
-        init = State("init", None, None, None)
-        second = State("second", None, None, None)
         transition1 = Transition("START", "init", "script1.sh", 10)
         transition2 = Transition("START", "second", "script2.sh", 12)
-        first_transition = get_plan(["second"], [init, second], [transition1, transition2])[0]
+        first_transition = get_transitions({"second"}, [transition1, transition2])[0]
         self.assertEqual(first_transition.from_state, "START")
         self.assertEqual(first_transition.to_state, "second")
         self.assertEqual(first_transition.script_to_run, "script2.sh")
+
+    def test_check_populate(self):
+        transition1 = Transition("START", "init", "python3 sleep.py", 10)
+        transitions = populate_transitions([transition1])
+        self.assertIsNotNone(transitions[0].weight)
 
 if __name__ == '__main__':
     unittest.main()
--- state.py
+++ state.py
@@ -1,13 +1,15 @@
-# This is a sample Python script.
+import os
+import time
+import mlrose_hiive
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 class State:
-    def __init__(self, state_name, tests, conflicting_state, required_state):
+    def __init__(self, state_name, tests, required_state):
         self.state_name = state_name
         self.tests = tests
-        self.conflicting_state = conflicting_state
         self.required_state = required_state
 
 
@@ -18,15 +20,51 @@ class Transition:
         self.weight = weight
         self.to_state = to_state
 
+
 #
-# A series of expected states sequences is given. e.g. [[state1], [state2, state3]]
 # Returns the used transitions.
-def get_plan(expected_states_sequence, available_states, transitions):
-    picked_transitions = []
-    for state_name in expected_states_sequence:
+def get_transitions_mlrose(states_to_visit, transitions):
+    state_list = []
+    dist_list = []
+    for transition in transitions:
+        if transition.from_state not in state_list:
+            state_list.append(transition.from_state)
+        if transition.to_state not in state_list:
+            state_list.append(transition.to_state)
+
+        dist_list.append(
+            (state_list.index(transition.from_state), state_list.index(transition.to_state), transition.weight))
+    fitness_dists = mlrose_hiive.TravellingSales(distances=dist_list)
+    problem_fit = mlrose_hiive.TSPOpt(length=len(states_to_visit), fitness_fn=fitness_dists,
+                                      maximize=False)
+    state, best_fitness, other = mlrose_hiive.genetic_alg(problem_fit)
+    return state.tolist()
+
+#
+# Returns the used transitions.
+def get_transitions(states_to_visit, transitions):
+    resulting_transitions = []
+    current_state = "START"
+    for state_to_visit in states_to_visit:
+        found_transition = False
         for transition in transitions:
-            if transition.to_state == state_name:
-                picked_transitions.append(transition)
+            if transition.from_state == current_state and transition.to_state == state_to_visit:
+                resulting_transitions.append(transition)
+                found_transition = True
+                break
 
-    return picked_transitions
+        if not found_transition:
+            raise Exception(f"no transition found for {state_to_visit}")
+    return resulting_transitions
 
+#
+# run the transitions.
+def populate_transitions(transitions):
+    for transition in transitions:
+        t1_start = time.time()
+        exitcode = os.system(transition.script_to_run)
+        t1_stop = time.time()
+        if exitcode != 0:
+            raise Exception(f"{transition.script_to_run} failed with exit code: {exitcode}")
+        transition.weight = t1_stop - t1_start
+    return transitions
